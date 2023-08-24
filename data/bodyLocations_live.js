5d68ab1479f2d44ed3a2e22e018d54dff218e3af
gonnavis <gonnavis@163.com>
2023-08-25 02:16:17 +0800

    Add bodyLocations_live.js ( same as bodyLocations.js )

---
+++ data/bodyLocations_live.js
@@ -0,0 +1,28 @@
+
+// Body locations can be called to receive all the body locations
+export const bodyLocations = [
+  {
+    "ID": 16,
+    "Name": "Abdomen, pelvis & buttocks"
+  },
+  {
+    "ID": 7,
+    "Name": "Arms & shoulder"
+  },
+  {
+    "ID": 15,
+    "Name": "Chest & back"
+  },
+  {
+    "ID": 6,
+    "Name": "Head, throat & neck"
+  },
+  {
+    "ID": 10,
+    "Name": "Legs"
+  },
+  {
+    "ID": 17,
+    "Name": "Skin, joints & general"
+  }
+]
