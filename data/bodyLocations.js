13701845fb92c7e23198beb625b8182a115aaf52
gonnavis <gonnavis@163.com>
2023-08-24 17:45:18 +0800

    Add data.

---
+++ data/bodyLocations.js
@@ -0,0 +1,26 @@
+export const bodyLocations = [
+  {
+    "ID": 16,
+    "Name": "Abdomen, pelvis & buttocks"
+  },
+  {
+    "ID": 7,
+    "Name": "Arms & shoulder"
+  },
+  {
+    "ID": 15,
+    "Name": "Chest & back"
+  },
+  {
+    "ID": 6,
+    "Name": "Head, throat & neck"
+  },
+  {
+    "ID": 10,
+    "Name": "Legs"
+  },
+  {
+    "ID": 17,
+    "Name": "Skin, joints & general"
+  }
+]
---
+++ data/issue.js
@@ -0,0 +1,12 @@
+const detailIssues = {
+  495: {
+    "Description": "Meteorism is a condition where the abdomen feels full and tight. In this situation, the belly may look swollen and distended.",
+    "DescriptionShort": "Meteorism describes the accumulation of gas in the abdomen or in the intestines, accompanied by distention. It is normally not a serious problem. Sometimes, over-the-counter medication is enough to relieve the symptoms. But if meteorism is caused by certain diseases, one should consult a doctor.",
+    "MedicalCondition": "Causes of meteorism include 1) swallowing air, 2) constipation, 3) gastro-esophageal reflux (GERD), 4) irritable bowel syndrome, 5) lactose intolerance or problems with digestion, 6) overeating, 7) small bowel bacterial overgrowth, 8) weight gain and 9) weak abdominal muscles. Certain diseases may also cause meteorism. These include ascites and tumors, celiac diseases, dumping syndrome, intestinal obstruction and when the pancreas does not produce enough digestive enzyme.",
+    "Name": "Bloated belly",
+    "PossibleSymptoms": "Abdominal pain",
+    "ProfName": "Meteorism",
+    "Synonyms": null,
+    "TreatmentDescription": "To relieve the symptom of the feeling of distention in the abdomen, there are multiple over-the-counter drugs. One can avoid meteorism by following some advice: 1) avoid carbonated drinks and food with high levels of fructose or sorbitol, 2) avoid food that can produce gas such as beans, cabbages and the like, 3) do not eat too quickly, 4) stop smoking and 5) work out. If meteorism is caused by an underlying disease, then one should visit a specialist to treat the disease first."
+  },
+}
