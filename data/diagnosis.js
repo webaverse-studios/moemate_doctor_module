fa757bb33b55bb0462859e42f6cd5205705f316c
gonnavis <gonnavis@163.com>
2023-08-24 22:05:51 +0800

    data

---
+++ data/diagnosis.js
@@ -0,0 +1,177 @@
+
+// _ Symptom IDs _ Age _ Gender
+export const diagnosis = {
+  _10_1985_Male: [
+    {
+      "Issue": {
+        "ID": 495,
+        "Name": "Bloated belly",
+        "Accuracy": 90,
+        "Icd": "R14",
+        "IcdName": "Flatulence and related conditions",
+        "ProfName": "Meteorism",
+        "Ranking": 1
+      },
+      "Specialisation": [
+        {
+          "ID": 15,
+          "Name": "General practice",
+          "SpecialistID": 0
+        }
+      ]
+    },
+    {
+      "Issue": {
+        "ID": 53,
+        "Name": "Constipation",
+        "Accuracy": 45,
+        "Icd": "K59.0",
+        "IcdName": "Constipation",
+        "ProfName": "Obstipation",
+        "Ranking": 2
+      },
+      "Specialisation": [
+        {
+          "ID": 14,
+          "Name": "Gastroenterology",
+          "SpecialistID": 0
+        },
+        {
+          "ID": 15,
+          "Name": "General practice",
+          "SpecialistID": 0
+        },
+        {
+          "ID": 19,
+          "Name": "Internal medicine",
+          "SpecialistID": 0
+        }
+      ]
+    },
+    {
+      "Issue": {
+        "ID": 281,
+        "Name": "Food poisoning",
+        "Accuracy": 33.75,
+        "Icd": "A05;A02;A03;A04",
+        "IcdName": "Other bacterial foodborne intoxications, not elsewhere classified;Other salmonella infections;Shigellosis;Other bacterial intestinal infections",
+        "ProfName": "Foodborne illness",
+        "Ranking": 3
+      },
+      "Specialisation": [
+        {
+          "ID": 15,
+          "Name": "General practice",
+          "SpecialistID": 0
+        },
+        {
+          "ID": 19,
+          "Name": "Internal medicine",
+          "SpecialistID": 0
+        }
+      ]
+    },
+    {
+      "Issue": {
+        "ID": 51,
+        "Name": "Loose watery stools",
+        "Accuracy": 22.5,
+        "Icd": "A09;K59.1;K52.9",
+        "IcdName": "Other gastroenteritis and colitis of infectious and unspecified origin;Functional diarrhoea;Noninfective gastroenteritis and colitis, unspecified",
+        "ProfName": "Diarrhea",
+        "Ranking": 4
+      },
+      "Specialisation": [
+        {
+          "ID": 14,
+          "Name": "Gastroenterology",
+          "SpecialistID": 0
+        },
+        {
+          "ID": 15,
+          "Name": "General practice",
+          "SpecialistID": 0
+        },
+        {
+          "ID": 19,
+          "Name": "Internal medicine",
+          "SpecialistID": 0
+        }
+      ]
+    },
+    {
+      "Issue": {
+        "ID": 324,
+        "Name": "Kidney stones",
+        "Accuracy": 9.99,
+        "Icd": "N20",
+        "IcdName": "Calculus of kidney and ureter",
+        "ProfName": "Nephrolith",
+        "Ranking": 5
+      },
+      "Specialisation": [
+        {
+          "ID": 15,
+          "Name": "General practice",
+          "SpecialistID": 0
+        },
+        {
+          "ID": 42,
+          "Name": "Urology",
+          "SpecialistID": 0
+        }
+      ]
+    },
+    {
+      "Issue": {
+        "ID": 59,
+        "Name": "Urinary tract infection",
+        "Accuracy": 9.28125,
+        "Icd": "N39.0",
+        "IcdName": "Urinary tract infection, site not specified",
+        "ProfName": "Cystitis",
+        "Ranking": 6
+      },
+      "Specialisation": [
+        {
+          "ID": 15,
+          "Name": "General practice",
+          "SpecialistID": 0
+        },
+        {
+          "ID": 18,
+          "Name": "Gynecology",
+          "SpecialistID": 0
+        },
+        {
+          "ID": 19,
+          "Name": "Internal medicine",
+          "SpecialistID": 0
+        }
+      ]
+    },
+    {
+      "Issue": {
+        "ID": 130,
+        "Name": "Hernia",
+        "Accuracy": 9.1125,
+        "Icd": "K40;K41;K42;K43;K44;K45;K46",
+        "IcdName": "Inguinal hernia;Femoral hernia;Umbilical hernia;Ventral hernia;Diaphragmatic hernia;Other abdominal hernia;Unspecified abdominal hernia",
+        "ProfName": "Abdominal hernia",
+        "Ranking": 7
+      },
+      "Specialisation": [
+        {
+          "ID": 15,
+          "Name": "General practice",
+          "SpecialistID": 0
+        },
+        {
+          "ID": 39,
+          "Name": "Surgery",
+          "SpecialistID": 0
+        }
+      ]
+    }
+  ],
+}
