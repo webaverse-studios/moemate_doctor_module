dd991d36abc429b2db0460419a90c37d31e2a1e9
gonnavis <gonnavis@163.com>
2023-08-25 02:22:26 +0800

    Add issue_live.js.

---
+++ data/issue_live.js
@@ -0,0 +1,24 @@
+
+// Issue info can be called to receive all information about a health issue. The short description gives a short overview. A longer information can consist of "Description", "MedicalCondition", "TreatmentDescription".
+const detailIssues = {
+  41: {
+    "Description": "Sinusitis is an inflammation of one or more paranasal sinuses. It can either develop as a common sinus infection (rhinitis, a cold) or less commonly in conjunction with allergies. If sinusitis lasts longer than two months, it is referred to as being chronic. The maxillary sinus is often affected as well in acute or chronic infections. Because of the swelling of the mucosa in the paranasal sinuses, its secretions can no longer flow out properly and they become lodged in the paranasal sinuses. There they can become infected with pathogens (such as viruses).",
+    "DescriptionShort": "Sinusitis occurs when the cavities of the nose become inflamed and swollen. The condition is usually caused by a virus and  improves often within a short period of time maximum up to three weeks. It is less commonly coming in combination with an allergy.",
+    "MedicalCondition": "Sinusitis is relatively common. People with sinusitis often experience pressing or pulsating headaches around the forehead and eyes, which become stronger when bending forward. Furthermore, a runny nose (rhinitis), coughing at night, and a generally ill feeling and slight fever may occur. Chronic sinusitis may be accompanied by fatigue and exhaustion.",
+    "Name": "Inflammation of paranasal sinuses",
+    "PossibleSymptoms": "Sputum,Feeling of pressure in the ear,Fever,Face pain,Cough,Headache,Nosebleed,Runny nose,Stuffy nose,Neck pain,Toothache,Cough with sputum,Feeling ill",
+    "ProfName": "Sinusitis",
+    "Synonyms": null,
+    "TreatmentDescription": "Sinusitis can be diagnosed by your doctor in an examination and, if necessary, imaging can be used (such as a computer tomography or CT scan). This imaging can be used to discover polyps in the sinuses. Treatment consists of cleansing the nose with salt water, nasal sprays with cortisone, and in certain cases the use of antibiotics. Severe cases require treatment by an ENT (Ear Nose & Throat) specialist. Polyps can be completely removed in a minor surgery."
+  },
+  495: {
+    "Description": "Meteorism, called also tympanites, is a condition where the abdomen feels full and tight. In this situation, the abdomen may look swollen and distended. Meteorism is caused by the increase in the quantity of intestinal gas, applying more pressure than usual inside the digestive system and generating various discomforts. Particularly there usually is a rather intense flatulence and pain caused by the elimination process of the gas. Simple dietary changes are usually enough to make the annoying symptoms disappear. Sometimes, doctors can recommend the use of certain drugs.",
+    "DescriptionShort": "Meteorism describes the accumulation of gas in the abdomen or in the intestines, accompanied by distention. It is normally not a serious problem. Sometimes, over-the-counter medication is enough to relieve the symptoms. But if meteorism is caused by certain diseases, one should consult a doctor.",
+    "MedicalCondition": "Causes of meteorism include 1) swallowing air, 2) constipation, 3) gastro-esophageal reflux (GERD), 4) irritable bowel syndrome, 5) lactose intolerance or problems with digestion, 6) overeating, 7) small bowel bacterial overgrowth, 8) weight gain and 9) weak abdominal muscles. Certain diseases may also cause meteorism. These include ascites and tumors, celiac diseases, dumping syndrome, intestinal obstruction and when the pancreas does not produce enough digestive enzyme.",
+    "Name": "Bloated belly",
+    "PossibleSymptoms": "Flatulence,Abdominal pain,Bloated feeling in the stomach,Cramps,Distended abdomen",
+    "ProfName": "Meteorism",
+    "Synonyms": null,
+    "TreatmentDescription": "To relieve the symptom of the feeling of distention in the abdomen, there are multiple over-the-counter drugs. One can avoid meteorism by following some advice: 1) avoid carbonated drinks and food with high levels of fructose or sorbitol, 2) avoid food that can produce gas such as beans, cabbages and the like, 3) do not eat too quickly, 4) stop smoking and 5) work out. If meteorism is caused by an underlying disease, then one should visit a specialist to treat the disease first."
+  },
+}
