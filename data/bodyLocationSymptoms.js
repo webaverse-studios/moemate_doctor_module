d6ad00d8165c1598d47cb53092da90938e0274bb
gonnavis <gonnavis@163.com>
2023-08-30 21:02:34 +0800

    data

---
+++ data/bodyLocationSymptoms.js
@@ -0,0 +1,124 @@
+
+
+// _ Body Location ID _ Gender and Age (Male / Female / Boy / Girl)
+export const bodyLocationSymptoms = {
+  _16_male: [
+    {
+      "ID": 10,
+      "Name": "Abdominal pain",
+      "HasRedFlag": false,
+      "HealthSymptomLocationIDs": [
+        16,
+        36
+      ],
+      "ProfName": "",
+      "Synonyms": [
+        " Stomach ache"
+      ]
+    },
+    {
+      "ID": 44,
+      "Name": "Nausea",
+      "HasRedFlag": false,
+      "HealthSymptomLocationIDs": [
+        6,
+        22,
+        15,
+        31,
+        16,
+        36,
+        17,
+        47
+      ],
+      "ProfName": "",
+      "Synonyms": []
+    },
+    {
+      "ID": 45,
+      "Name": "Heartburn",
+      "HasRedFlag": false,
+      "HealthSymptomLocationIDs": [
+        6,
+        24,
+        15,
+        31,
+        16,
+        36
+      ],
+      "ProfName": "",
+      "Synonyms": [
+        "Pyrosis"
+      ]
+    },
+    {
+      "ID": 92,
+      "Name": "Early satiety",
+      "HasRedFlag": false,
+      "HealthSymptomLocationIDs": [
+        16,
+        36
+      ],
+      "ProfName": "",
+      "Synonyms": []
+    },
+    {
+      "ID": 101,
+      "Name": "Vomiting",
+      "HasRedFlag": false,
+      "HealthSymptomLocationIDs": [
+        6,
+        25,
+        16,
+        36
+      ],
+      "ProfName": "",
+      "Synonyms": []
+    },
+    {
+      "ID": 153,
+      "Name": "Fast, deepened breathing",
+      "HasRedFlag": false,
+      "HealthSymptomLocationIDs": [
+        6,
+        24,
+        25,
+        15,
+        31,
+        16,
+        36
+      ],
+      "ProfName": "",
+      "Synonyms": [
+        "Fast breathing",
+        "Hyperventilation"
+      ]
+    },
+    {
+      "ID": 179,
+      "Name": "Stomach burning",
+      "HasRedFlag": false,
+      "HealthSymptomLocationIDs": [
+        16,
+        36
+      ],
+      "ProfName": "",
+      "Synonyms": [
+        "Burning abdominal pain",
+        "Burning stomach ache"
+      ]
+    },
+    {
+      "ID": 181,
+      "Name": "Vomiting blood",
+      "HasRedFlag": true,
+      "HealthSymptomLocationIDs": [
+        6,
+        25,
+        16,
+        36
+      ],
+      "ProfName": "",
+      "Synonyms": []
+    }
+  ],
+}
--- data/bodySublocations.js
+++ data/bodySublocations.js
@@ -23,4 +23,102 @@ export const bodySublocations = {
       "Name": "Pelvis"
     }
   ],
+  7: [
+    {
+      "ID": 48,
+      "Name": "Arms general"
+    },
+    {
+      "ID": 30,
+      "Name": "Finger"
+    },
+    {
+      "ID": 28,
+      "Name": "Forearm & wrist"
+    },
+    {
+      "ID": 29,
+      "Name": "Hand & wrist"
+    },
+    {
+      "ID": 26,
+      "Name": "Upper arm & shoulder"
+    }
+  ],
+  15: [
+    {
+      "ID": 33,
+      "Name": "Back"
+    },
+    {
+      "ID": 31,
+      "Name": "Chest"
+    },
+    {
+      "ID": 32,
+      "Name": "Lateral chest"
+    },
+    {
+      "ID": 35,
+      "Name": "Lower back"
+    },
+    {
+      "ID": 34,
+      "Name": "Spine"
+    }
+  ],
+  6: [
+    {
+      "ID": 23,
+      "Name": "Face & eyes"
+    },
+    {
+      "ID": 22,
+      "Name": "Forehead & head in general"
+    },
+    {
+      "ID": 21,
+      "Name": "Hair & scalp"
+    },
+    {
+      "ID": 25,
+      "Name": "Mouth & jaw"
+    },
+    {
+      "ID": 24,
+      "Name": "Nose, ears, throat & neck"
+    }
+  ],
+  10: [
+    {
+      "ID": 44,
+      "Name": "Foot"
+    },
+    {
+      "ID": 49,
+      "Name": "Legs general"
+    },
+    {
+      "ID": 43,
+      "Name": "Lower leg & ankle"
+    },
+    {
+      "ID": 41,
+      "Name": "Thigh & knee"
+    },
+    {
+      "ID": 45,
+      "Name": "Toes"
+    }
+  ],
+  17: [
+    {
+      "ID": 47,
+      "Name": "General, joints & other"
+    },
+    {
+      "ID": 46,
+      "Name": "Skin"
+    }
+  ],
 }
