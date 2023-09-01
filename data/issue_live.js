3496b10e50e0818bcb5452744e83bca5487f8bc1
gonnavis <gonnavis@163.com>
2023-09-02 05:18:11 +0800

    data

--- data/issue_live.js
+++ data/issue_live.js
@@ -11,6 +11,46 @@ const detailIssues = {
     "Synonyms": null,
     "TreatmentDescription": "Sinusitis can be diagnosed by your doctor in an examination and, if necessary, imaging can be used (such as a computer tomography or CT scan). This imaging can be used to discover polyps in the sinuses. Treatment consists of cleansing the nose with salt water, nasal sprays with cortisone, and in certain cases the use of antibiotics. Severe cases require treatment by an ENT (Ear Nose & Throat) specialist. Polyps can be completely removed in a minor surgery."
   },
+  139: {
+    "Description": "Blepharitis happens generally when oil glands of small size, which are situated close to the bottom of the eyelashes, get congested. This results in redness and irritation of the eye. It is difficult to treat Blepharitis and it is often a chronic disease. Blepharitis can be unattractive and uncomfortable but it is not contagious and usually doesn't trigger permanent harm to the eyesight.",
+    "DescriptionShort": "Blepharitis is a general disease characterized by swelling and redness of the margins limiting the eyelids. The condition thus usually involves the eyelid’s area used by the eyelashes to grow out. In most cases both eyes are affected, but one eye can be more affected than the other and the symptoms seem to be worse in the morning.",
+    "MedicalCondition": "Patients experience a swelling of the lids, which may become stuck together, as well as the loss of eyelashes. Blepharitis can produce open wounds with yellow crust and multiple pustules. Patients often complain of feeling a foreign object in the eye, burning, and itching in the infected eye.",
+    "Name": "Eyelid inflammation",
+    "PossibleSymptoms": "Eyelid swelling,Eyelids sticking together,Eye redness,Burning eyes,Itching eyes,Loss of eye lashes,Eye pain",
+    "ProfName": "Blepharitis",
+    "Synonyms": null,
+    "TreatmentDescription": "Antibiotic creams are often applied in the treatment of blepharitis. Blepharitis as it occurs in conjunction with skin disorders can also be treated with regular, basic eyelid hygiene, which involves massaging out the sebaceous glands and cleaning the ducts."
+  },
+  140: {
+    "Description": "The conjunctiva covers the eye where it comes in contact with the environment, as well as the inside part of the upper and lower eyelids. Irritation and redness in the conjunctiva occur when there is an allergic reaction, a disturbance to the formation or flow of tears, or infection by viruses or bacteria.",
+    "DescriptionShort": "Conjunctivitis is a general condition that triggers inflammation and redness of the conjunctiva which comes with a burning sensation in the eye. Conjunctiva is a thin coating of tissue that protects the eye at the front. At first, the condition can affect just one eye, but after some hours conjunctivitis usually affects both eyes. Reasons for this may include: an allergy, physical or chemical irritants, viruses or bacteria.",
+    "MedicalCondition": "Inflammation of the conjunctiva causes an increase in blood flow and therefore reddening of the eye. The conjunctiva may also swell. Furthermore, the patient suffers from an increase in tear production, sensitivity to light, urge to scratch, a burning sensation, and the sensation of a foreign object in the eye, as well as severe pain.",
+    "Name": "Pink eye",
+    "PossibleSymptoms": "Eye redness,Feeling of foreign body in the eye,Itching eyes,Burning eyes,Tears,Eyelids sticking together,Oversensitivity to light,Eye pain",
+    "ProfName": "Conjunctivitis",
+    "Synonyms": "Pink-eye,Pinkeye",
+    "TreatmentDescription": "Depending on the cause, conjunctivitis can be treated with antibiotics or cortisone droplets. If the tear film is impaired, it needs to be supported by a sufficient and high quality substitute in form of eye drops."
+  },
+  214: {
+    "Description": "Dacryocystitis may be chronic or acute. For acute infection, the area surrounding the nasolacrimal bag is swollen, red, and painful. The area around the eye may become watery and red and can leak pus. Dacryocystitis is caused by an infection of the nasolacrimal bag that leads to a blocking of the lacrimal drainage system. This condition can occur later in life or can be inherited.",
+    "DescriptionShort": "Dacrocystitis is an inflammation of the nasolacrimal bag which develops when tears cannot flow freely from the inner corner of the eye and is subsequently colonized with bacteria. This bacterial colonization caused by the impaired outflow leads to the inflammation.",
+    "MedicalCondition": "People with dacrocystitis suffer from pain, swelling, and redness in the corner of the eye up to the root of the nose. Pus is sometimes discharged from the nasolacrimal bag into the eye.",
+    "Name": "Lacrimal gland inflammation",
+    "PossibleSymptoms": "Tears,Eyelids sticking together,Face pain,Eye blinking",
+    "ProfName": "Dacrocystitis",
+    "Synonyms": null,
+    "TreatmentDescription": "Dacrocystitis can be treated with antibiotics and moist, disinfected compress. If the nasolacrimal sac is swollen to the point that an abscess could form, then it can be opened with a small incision and a drain can be inserted for several days. Since the tear duct remains clogged and closed after an episode of dacrocystitis, an operation should follow in order to reopen the tear duct and prevent future inflammation."
+  },
+  450: {
+    "Description": "A stye results from the obstruction of a gland near the eyelid with subsequent infection. It may cause a pus-filled, red lump to form around the eyelid. A stye often forms on the outside part of the upper eyelid. However, it may sometimes form on the inner part of the eyelid. A stye looks similar to a chalazion, which is characterized as a non-infectious and mostly painless obstruction of a gland.",
+    "DescriptionShort": "A stye is an inflammatory lump on the eyelid that is caused by a bacterial infection of a gland. The reason for this infection is often an obstruction of a gland with subsequent infection. Sometime a sty will spontaneously heal, but sometimes it will need to be treated by antibiotics or a small surgical intervention.",
+    "MedicalCondition": "The underlying cause of a stye is often an obstruction of a gland near the eyelid. A lump near the eyelid due to obstruction of the gland without infection is classified as a chalazion. Besides, a stye forms mostly in sweat and sebaceous glands, while a chalazion is caused by the obstruction of a meibomian gland. However, stowed secretion provides ideal nutrients for bacteria. Once infected with bacteria, a gland becomes a growing inflammatory lump on the eyelid, which is classified as a stye. Behind this lump, there is a pus-filled cavity (abscess), which may drain from time to time. People with a stye may experience a red lump, mostly near the outside part of the upper eyelid, which may cause pain and sometimes swelling of the eyelid. Due to the obstruction of certain glands, dry eye symptoms may also occur in some cases. Untreated the abscess can spread on the whole eyelid, on the conjunctives or even behind the eye.",
+    "Name": "Stye",
+    "PossibleSymptoms": "Eyelid swelling,Eyelids sticking together,Eye pain",
+    "ProfName": "Hordeolum",
+    "Synonyms": "Sty",
+    "TreatmentDescription": "The treatment of a stye will depend on the condition. A small stye can heal by itself. However, medications such as antibiotics will help to eradicate bacteria. It is important to avoid squeezing the sties to prevent a further infection. In difficult cases, incision and drainage of pus may be considered."
+  },
   495: {
     "Description": "Meteorism, called also tympanites, is a condition where the abdomen feels full and tight. In this situation, the abdomen may look swollen and distended. Meteorism is caused by the increase in the quantity of intestinal gas, applying more pressure than usual inside the digestive system and generating various discomforts. Particularly there usually is a rather intense flatulence and pain caused by the elimination process of the gas. Simple dietary changes are usually enough to make the annoying symptoms disappear. Sometimes, doctors can recommend the use of certain drugs.",
     "DescriptionShort": "Meteorism describes the accumulation of gas in the abdomen or in the intestines, accompanied by distention. It is normally not a serious problem. Sometimes, over-the-counter medication is enough to relieve the symptoms. But if meteorism is caused by certain diseases, one should consult a doctor.",
